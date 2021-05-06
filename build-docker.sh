b6f061f08570e48fb60e82b873a779a725f3c6ba
Marco Oesch <marco.oesch@decentage.com>
2021-05-06 17:15:06 +0200

    updated build docker script

--- build-docker.sh
+++ build-docker.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-docker build -t decentage/iep-blockexplorer-ui:latest  .
+set -o errexit
+set -o pipefail
+set -o nounset
+
+RELEASE_VERSION=$(cat release-version.txt)
+docker build --build-arg RELEASE_VERSION=${RELEASE_VERSION} -t decentage/iep-blockexplorer-ui:latest  .
