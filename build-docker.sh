b3c819191b71f6b0b662a269f29baf0c80f6c0d4
mazila <mazila@mazila-xps159560>
2021-06-01 13:58:39 +0200

    update

--- build-docker.sh
+++ build-docker.sh
@@ -6,7 +6,7 @@ set -o nounset
 RELEASE_VERSION=$(cat release-version.txt)
 docker build -t decentage/iep-blockexplorer-ui:${RELEASE_VERSION}  .
 
-CONTAINER_ID=$(docker create --rm --name iep-blockexplorer-ui-extract decentage/iep-blockexplorer-ui.zip:${RELEASE_VERSION})
+CONTAINER_ID=$(docker create --rm --name iep-blockexplorer-ui-extract decentage/iep-blockexplorer-ui:${RELEASE_VERSION})
 mkdir -p ./build
 docker cp ${CONTAINER_ID}:/build/iep-blockexplorer-ui.zip ./build
 docker rm ${CONTAINER_ID}
