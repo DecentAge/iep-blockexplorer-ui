f8a58a5fd0ddeac3257e343f805768a231332e4d
DecentAgeCoder <marco.oesch@decentage.com>
2021-11-28 19:05:16 +0100

    updated

--- build-docker.sh
+++ build-docker.sh
@@ -4,10 +4,10 @@ set -o pipefail
 set -o nounset
 
 RELEASE_VERSION=$(cat release-version.txt)
+chmod ugo+x 30-nginx-iep-startup-script.sh
 docker build -t decentage/iep-blockexplorer-ui:${RELEASE_VERSION}  .
 
 CONTAINER_ID=$(docker create --rm --name iep-blockexplorer-ui-extract decentage/iep-blockexplorer-ui:${RELEASE_VERSION})
 mkdir -p ./build
-chmod ugo+x 30-nginx-iep-startup-script.sh
 docker cp ${CONTAINER_ID}:/build/iep-blockexplorer-ui.zip ./build
 docker rm ${CONTAINER_ID}
