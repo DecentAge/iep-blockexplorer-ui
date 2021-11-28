48f6df07de60c7f475cf783020dc65f21ffb3c30
DecentAgeCoder <marco.oesch@decentage.com>
2021-11-28 18:58:51 +0100

    Updated chmod for startup script

--- build-docker.sh
+++ build-docker.sh
@@ -8,5 +8,6 @@ docker build -t decentage/iep-blockexplorer-ui:${RELEASE_VERSION}  .
 
 CONTAINER_ID=$(docker create --rm --name iep-blockexplorer-ui-extract decentage/iep-blockexplorer-ui:${RELEASE_VERSION})
 mkdir -p ./build
+chmod ugo+x 30-nginx-iep-startup-script.sh
 docker cp ${CONTAINER_ID}:/build/iep-blockexplorer-ui.zip ./build
 docker rm ${CONTAINER_ID}
