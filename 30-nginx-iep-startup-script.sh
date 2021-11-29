7ca9e3f479f661bebd50c9d1435739719e4a7486
DecentAgeCoder <marco.oesch@decentage.com>
2021-11-29 10:57:23 +0100

    updated

--- 30-nginx-iep-startup-script.sh
+++ 30-nginx-iep-startup-script.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
-set -o nounset
-set -o errexit
-set -o pipefail
-set -a
+set -eu
 
 echo "Setting environment variables /usr/share/nginx/html/env.config.js using template /etc/nginx/templates/env.config.js.template"
 envsubst < /etc/nginx/templates/env.config.js.template > /usr/share/nginx/html/env.config.js
